d5398b1d2af56a5274abc702aeb097f3f2f41d0e
TZEEE <35172094+TZEEE3124@users.noreply.github.com>
2024-09-17 19:49:53 +0800

    go

--- javascripts/discourse/components/sticky-sidebar.js
+++ javascripts/discourse/components/sticky-sidebar.js
@@ -1,8 +1,10 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default class StickySidebar extends Component {
+  @service router;
   @tracked top = 0;
   @tracked bottom = 0;
   @tracked position = "relative";
